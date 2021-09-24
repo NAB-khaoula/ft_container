8342801f8173067c8f856b40778b1bb25d32f55d
Khaoula Naboussi <k12.naboussi@gmail.com>
2021-09-24 11:37:07 +0100

    first push

---
+++ vector.hpp
@@ -0,0 +1,23 @@
+#ifdef VECTOR_HPP
+#define VECTOR_HPP
+
+template <typename T>
+class vector
+{
+private:
+    /* data */
+public:
+    vector(/* args */);
+    ~vector();
+};
+
+vector::vector(/* args */)
+{
+}
+
+vector::~vector()
+{
+}
+
+
+#endif
