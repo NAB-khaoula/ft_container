0d8771f23c2ecba4c96cfe8151eef9a9e98b7883
Khaoula Naboussi <knabouss@e1r8p4.1337.ma>
2021-09-28 11:21:24 +0100

    redone

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
 #include <iterator>
 #include <vector>
+#include <iostream>
+// #include "vector.hpp"
 
 int main(){
-    std::random_access_iterator_tag iterator;
     std::vector<int> vect;
+    vect.push_back(1337);
+    vect.push_back(42);
+    vect.push_back(19);
+    std::cout << vect[0] << std::endl;
+    std::cout << vect[1] << std::endl;
+    std::cout << vect[2] << std::endl;
+    std::vector<int> v2(vect.begin(), vect.end());
+    std::cout << v2[0] << std::endl;
+    std::cout << v2[1] << std::endl;
+    std::cout << v2[2] << std::endl;
+    // ft::vector<int> v3(vect.begin(), vect.end());
+    // std::cout << vect.capacity() << std::endl;
+    // std::cout << tester.capacity() << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -2,48 +2,71 @@
 #define VECTOR_HPP
 #include <memory>
 
+namespace ft{
 template <typename T, typename Alloc = std::allocator<T> >
-class ft_vector
+class vector
 {
-	private:
-		T*	_array;
-		int	_current;
-		int _capacity;
 	public:
 		//****************** member Type ******************************
 		typedef	T					value_type;
-		typedef	Alloc<value_type>	allocator_type;
-		allocator_type		&reference;
-		const allocator_type	&const_reference;
-		allocator_type		*pointer;
-		const allocator_type	*const_pointer;
-		std::random_access_iterator_tag	iterator;
-		const std::random_access_iterator_tag	const_iterator;
-		std::reverse_iterator<iterator>	reverse_iterator;
-		std::reverse_iterator<const_iterator> const_reverse_iterator;
-		std::iterator_traits<iterator>	difference_type;
+		typedef	Alloc				allocator_type;
+		// allocator_type		&reference;
+		// const allocator_type	&const_reference;
+		// allocator_type		*pointer;
+		// const allocator_type	*const_pointer;
+		// std::random_access_iterator_tag	iterator;
+		// const std::random_access_iterator_tag	const_iterator;
+		// std::reverse_iterator<iterator>	reverse_iterator;
+		// std::reverse_iterator<const_iterator> const_reverse_iterator;
+		// std::iterator_traits<iterator>	difference_type;
 		typedef	size_t		size_type;
 		//****************** member Type ******************************
-		ft_vector(const allocator_type& alloc = allocator_type()){
+		vector(const allocator_type& alloc = allocator_type()){
+			_allocator = alloc;
 			_array = nullptr;
 			_current = 0;
 			_capacity = 0;
 		}
-		ft_vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()){
-			_array = std::allocate(n);
+		vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()){
+			_allocator = alloc;
+			_array = _allocator.allocate(n);
 			_current = 0;
 			_capacity = n;
 		}
 		template <class InputIterator>
-        ft_vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
-			
+        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
+			int i = 0;
+			_allocator = alloc;
+			while (first != last)
+			{
+				i++;
+				first++;
+			}
+			_current = 0;
+			_capacity = i;
+			_array = _allocator.allocate(i);
 		}
-		ft_vector (const ft_vector& x){
+		vector (const vector& x){
 			(*this) = x;
 		}
-		~ft_vector(){
-			std::deallocate(_array);
+		vector& operator= (const vector& x){
+			_array = x._array;
+			_current = x._current;
+			_capacity = x._capacity;
+			_allocator = x._allocator;
 		}
+		~vector(){
+			_allocator.deallocate(_array, _capacity);
+		}
+		size_type capacity() const{
+			return _capacity;
+		}
+	private:
+		T*			_array;
+		int			_current;
+		size_type 	_capacity;
+		Alloc		_allocator;
 };
 
+}
 #endif
