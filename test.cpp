665306238a943fa7d8cbadc2963241780d8dde22
Khaoula Naboussi <knabouss@e1r8p4.1337.ma>
2021-09-28 09:46:06 +0100

    redone

---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iterator>
+#include <vector>
+
+int main(){
+    std::random_access_iterator_tag iterator;
+}
--- vector.hpp
+++ vector.hpp
@@ -1,23 +1,29 @@
-#ifdef VECTOR_HPP
+#ifndef VECTOR_HPP
 #define VECTOR_HPP
+#include <memory>
 
-template <typename T>
+template <typename T, typename Alloc = std::allocator<T> >
 class vector
 {
-private:
-    /* data */
-public:
-    vector(/* args */);
-    ~vector();
+	private:
+		T*	_array;
+		int	_current;
+		int _capacity;
+	public:
+		//****************** member Type ******************************
+		T					value_type;
+		Alloc<valueType>	allocator_type;
+		allocator_type		&reference;
+		const allocator_type	&const_reference;
+		allocator_type		*pointer;
+		const allocator_type	*const_pointer;
+		std::random_access_iterator_tag	iterator;
+		const std::random_access_iterator_tag	const_iterator;
+		std::reverse_iterator<iterator>	reverse_iterator;
+		std::reverse_iterator<const_iterator> const_reverse_iterator;
+		//****************** member Type ******************************
+		vector();
+		~vector();
 };
-
-vector::vector(/* args */)
-{
-}
-
-vector::~vector()
-{
-}
-
 
 #endif
