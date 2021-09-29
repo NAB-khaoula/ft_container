80c05bad05f62e19c4e33fbd7b84ef31a9bd32bf
Khaoula Naboussi <knabouss@e2r4p14.1337.ma>
2021-09-29 13:37:31 +0100

    first push

--- test.cpp
+++ test.cpp
@@ -1,21 +1,19 @@
 #include <iterator>
 #include <vector>
 #include <iostream>
-// #include "vector.hpp"
+#include "vector.hpp"
 
 int main(){
-    std::vector<int> vect;
-    vect.push_back(1337);
-    vect.push_back(42);
-    vect.push_back(19);
-    std::cout << vect[0] << std::endl;
-    std::cout << vect[1] << std::endl;
-    std::cout << vect[2] << std::endl;
-    std::vector<int> v2(vect.begin(), vect.end());
-    std::cout << v2[0] << std::endl;
-    std::cout << v2[1] << std::endl;
-    std::cout << v2[2] << std::endl;
-    // ft::vector<int> v3(vect.begin(), vect.end());
-    // std::cout << vect.capacity() << std::endl;
-    // std::cout << tester.capacity() << std::endl;
+    std::vector<int> vect(3);
+    std::cout << vect.size() << std::endl;
+    std::cout << vect.capacity() << std::endl;
+    std::cout << vect.max_size() << std::endl;
+    std::cout << vect.empty() << std::endl;
+    std::cout << "*********************" << std::endl;
+    ft::vector<int> myVect(3);
+    std::cout << myVect.size() << std::endl;
+    std::cout << myVect.capacity() << std::endl;
+    std::cout << myVect.max_size() << std::endl;
 }
+
+
--- vector.hpp
+++ vector.hpp
@@ -1,6 +1,7 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 #include <memory>
+#include <math.h>
 
 namespace ft{
 template <typename T, typename Alloc = std::allocator<T> >
@@ -14,13 +15,14 @@ class vector
 		// const allocator_type	&const_reference;
 		// allocator_type		*pointer;
 		// const allocator_type	*const_pointer;
-		std::random_access_iterator_tag	iterator;
+		typedef std::random_access_iterator_tag	iterator(value_type);
 		// const std::random_access_iterator_tag	const_iterator;
 		// std::reverse_iterator<iterator>	reverse_iterator;
 		// std::reverse_iterator<const_iterator> const_reverse_iterator;
 		// std::iterator_traits<iterator>	difference_type;
 		typedef	size_t		size_type;
 		//****************** member Type ******************************
+		//******************constructor******************
 		vector(const allocator_type& alloc = allocator_type()){
 			_allocator = alloc;
 			_array = nullptr;
@@ -33,34 +35,55 @@ class vector
 			_current = 0;
 			_capacity = n;
 		}
-		template <class InputIterator>
-        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
-			int i = 0;
-			_allocator = alloc;
-			while (first != last)
-			{
-				i++;
-				first++;
-			}
-			_current = 0;
-			_capacity = i;
-			_array = _allocator.allocate(i);
-		}
+		// template <class InputIterator>
+        // vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
+		// 	int i = 0;
+		// 	_allocator = alloc;
+		// 	while (first != last)
+		// 	{
+		// 		i++;
+		// 		first++;
+		// 	}
+		// 	_current = 0;
+		// 	_capacity = i;
+		// 	_array = _allocator.allocate(i);
+		// }
 		vector (const vector& x){
 			(*this) = x;
 		}
+		//******************constructor******************
+		//******************assignment operator******************
 		vector& operator= (const vector& x){
 			_array = x._array;
 			_current = x._current;
 			_capacity = x._capacity;
 			_allocator = x._allocator;
 		}
+		//******************assignment operator******************
+		//******************destructor******************
 		~vector(){
 			_allocator.deallocate(_array, _capacity);
 		}
+		//******************destructor******************
+		//******************method of Capacity******************
+		// need verification!!
 		size_type capacity() const{
+			if (!_current)
+				return _capacity;
+			else
+				return _current;
+		}
+		size_type size() const{
 			return _capacity;
 		}
+		size_type max_size() const{
+			size_type maxSize = pow(2,64)/sizeof(value_type);
+			return (maxSize - 1);
+		}
+		bool empty() const{
+			
+		}
+		//******************method of Capacity******************
 	private:
 		T*			_array;
 		int			_current;
