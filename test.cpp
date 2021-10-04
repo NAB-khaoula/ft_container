6de41d783bb4bc46c067ab96fbf7251ef3bf32c4
NAB-khaoula <k12.naboussi@gmail.com>
2021-10-04 12:08:41 +0100

    wokring on iterators

--- test.cpp
+++ test.cpp
@@ -3,17 +3,33 @@
 #include <iostream>
 #include "vector.hpp"
 
+struct ms
+{
+    int a;
+    int b;
+    ms(int a = 1,int  b = 2): a(a), b(b){
+        std::cout << "constructor called" << std::endl;
+    }
+    ms(ms &val): a(val.a), b(val.b){
+
+        std::cout << "copy constructor called" << std::endl;
+    }
+    ~ms(){}
+};
+
 int main(){
-    std::vector<int> vect(3);
-    std::cout << vect.size() << std::endl;
-    std::cout << vect.capacity() << std::endl;
-    std::cout << vect.max_size() << std::endl;
-    std::cout << vect.empty() << std::endl;
-    std::cout << "*********************" << std::endl;
-    ft::vector<int> myVect(3);
-    std::cout << myVect.size() << std::endl;
-    std::cout << myVect.capacity() << std::endl;
-    std::cout << myVect.max_size() << std::endl;
+    int *arr = new int(6);
+    ms m(3, 4);
+    std::vector<ms> vect(3);
+    // std::cout << vect.size() << std::endl;
+    // std::cout << vect.capacity() << std::endl;
+    // // std::cout << vect.max_size() << std::endl;
+    // // std::cout << vect.empty() << std::endl;
+    // std::cout << "*********************" << std::endl;
+    // ft::vector<int> myVect(3);
+    // std::cout << myVect.size() << std::endl;
+    // std::cout << myVect.capacity() << std::endl;
+    // std::cout << myVect.max_size() << std::endl;
 }
 
 
--- vector.hpp
+++ vector.hpp
@@ -3,40 +3,45 @@
 #include <memory>
 #include <math.h>
 
-namespace ft{
-template <typename T, typename Alloc = std::allocator<T> >
-class vector
+namespace ft
 {
+	template <typename T, typename Alloc = std::allocator<T> >
+	class vector
+	{
 	public:
 		//****************** member Type ******************************
-		typedef	T					value_type;
-		typedef	Alloc				allocator_type;
+		typedef T value_type;
+		typedef Alloc allocator_type;
 		// allocator_type		&reference;
 		// const allocator_type	&const_reference;
 		// allocator_type		*pointer;
 		// const allocator_type	*const_pointer;
-		typedef std::random_access_iterator_tag	iterator(value_type);
+		typedef std::random_access_iterator_tag iterator(value_type);
 		// const std::random_access_iterator_tag	const_iterator;
 		// std::reverse_iterator<iterator>	reverse_iterator;
 		// std::reverse_iterator<const_iterator> const_reverse_iterator;
 		// std::iterator_traits<iterator>	difference_type;
-		typedef	size_t		size_type;
+		typedef size_t size_type;
 		//****************** member Type ******************************
 		//******************constructor******************
-		vector(const allocator_type& alloc = allocator_type()){
+		vector(const allocator_type &alloc = allocator_type())
+		{
 			_allocator = alloc;
 			_array = nullptr;
 			_current = 0;
 			_capacity = 0;
 		}
-		vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()){
+		vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
+		{
 			_allocator = alloc;
 			_array = _allocator.allocate(n);
-			_current = 0;
+			for (size_type i = 0; i < n; i++)
+				_array[i] = _allocator.construct(val);
+			_current = n;
 			_capacity = n;
 		}
 		// template <class InputIterator>
-        // vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
+		// vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
 		// 	int i = 0;
 		// 	_allocator = alloc;
 		// 	while (first != last)
@@ -48,12 +53,14 @@ class vector
 		// 	_capacity = i;
 		// 	_array = _allocator.allocate(i);
 		// }
-		vector (const vector& x){
+		vector(const vector &x)
+		{
 			(*this) = x;
 		}
 		//******************constructor******************
 		//******************assignment operator******************
-		vector& operator= (const vector& x){
+		vector &operator=(const vector &x)
+		{
 			_array = x._array;
 			_current = x._current;
 			_capacity = x._capacity;
@@ -61,35 +68,36 @@ class vector
 		}
 		//******************assignment operator******************
 		//******************destructor******************
-		~vector(){
+		~vector()
+		{
 			_allocator.deallocate(_array, _capacity);
 		}
 		//******************destructor******************
 		//******************method of Capacity******************
 		// need verification!!
-		size_type capacity() const{
-			if (!_current)
-				return _capacity;
-			else
-				return _current;
-		}
-		size_type size() const{
+		size_type capacity() const
+		{
 			return _capacity;
 		}
-		size_type max_size() const{
-			size_type maxSize = pow(2,64)/sizeof(value_type);
+		size_type size() const
+		{
+			return _current;
+		}
+		size_type max_size() const
+		{
+			size_type maxSize = pow(2, 64) / sizeof(value_type);
 			return (maxSize - 1);
 		}
-		bool empty() const{
-			
+		bool empty() const
+		{
 		}
 		//******************method of Capacity******************
 	private:
-		T*			_array;
-		int			_current;
-		size_type 	_capacity;
-		Alloc		_allocator;
-};
+		T *_array;
+		int _current;
+		size_type _capacity;
+		Alloc _allocator;
+	};
 
 }
 #endif
