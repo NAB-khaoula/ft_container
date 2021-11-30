6a4638dcfd9b7fea6f0d3f88e96182ce8453bac1
NAB-khaoula <k12.naboussi@gmail.com>
2021-11-30 10:48:23 +0100

    deleting a node with balance

--- __treeBeta.hpp
+++ __treeBeta.hpp
@@ -14,7 +14,7 @@ namespace ft{
 		binaryTreeNode*	_parent;
 		binaryTreeNode*	_left;
 		binaryTreeNode*	_right;
-		// NOTE check it T() works well, no segfault
+		// NOTE check if T() works well, no segfault
 		binaryTreeNode(T& data = T()) : _data(data), _left(NULL), _right(NULL), _parent(NULL), height(1) {}
 	};
 
@@ -85,40 +85,34 @@ namespace ft{
 			return (a > b ? a : b);
 		}
 
-		void	rightRotation(binaryTreeNode<T> *node){
-			binaryTreeNode<T> *temp;
-			temp = node->_left;
-			std::cout << "debugging" << std::endl;
-			node->_left = temp->_right;
-			temp->_right->_parent = node;
-			temp->_right = node;
-			node->_parent = temp;
-			// FIXME not always the root!!
-			if(node == _root)
-				_root = temp;
-			else if (node->_parent->_left == node){
-				node->_parent->_left = temp;
-			}
-			else
-				node->_parent->_right = temp;
+		_node	*leftRotation(_node *node){
+			_node *temp;
+
+			temp = node;
+			node = temp->_right;
+			temp->_right = node->_left;
+			node->_left = temp;
+			temp->_parent = node;
+			node->_parent = temp->_parent;
+			if (temp->_right)
+			 	temp->_right->_parent = temp;
+			temp->height = 1 + (max(getHeight(temp->_left), getHeight(temp->_right)));
+			node->height = 1 + (max(getHeight(node->_left), getHeight(node->_right)));
+			return node;
 		}
 
-		void	leftRotation(binaryTreeNode<T> *node){
-			binaryTreeNode<T> *temp;
-
-			temp = node->_right;
-			node->_right = temp->_left;
-			temp->_left->_parent = node;
-			temp->_left = node;
-			node->_parent = temp;
-			// FIXME not always the root!!
-			if(node == _root)
-				_root = temp;
-			else if (node->_parent->_left == node){
-				node->_parent->_left = temp;
-			}
-			else
-				node->_parent->_right = temp;
+		_node	*rightRotation(_node *node){
+			_node *temp;
+			temp = node;
+			node = temp->_left;
+			temp->_left = node->_right;
+			node->_right = temp;
+			temp->_parent = node;
+			if (temp->_left)
+			 	temp->_left->_parent = temp;
+			temp->height = 1 + (max(getHeight(temp->_left), getHeight(temp->_right)));
+			node->height = 1 + (max(getHeight(node->_left), getHeight(node->_right)));
+			return node;
 		}
 
 		int	balanceFactor(_node *node){
@@ -128,29 +122,26 @@ namespace ft{
 		}
 
 		bool	leftheavy(_node *node){
-			int _balanceFactor = 0;
-			_balanceFactor = balanceFactor(node);
-			return (_balanceFactor > 1 && getHeight(node->_left) > getHeight(node->_right));
+			return (getHeight(node->_left) > getHeight(node->_right));
 		}
 
 		bool	rightheavy(_node *node){
-			int _balanceFactor = 0;
-			_balanceFactor = balanceFactor(node);
-			return (_balanceFactor > 1 && getHeight(node->_right) > getHeight(node->_left));
+			return (getHeight(node->_right) > getHeight(node->_left));
 		}
 
-		void	balanceTree(_node *node){
-			if (leftheavy(node))
+		_node	*balanceTree(_node *node){
+			if (balanceFactor(node) > 1)
 			{
-				// leftRotation(node);
-				// std::cout << node->_data.first << std::endl;
+				if (leftheavy(node))
+				{
+					node = rightRotation(node);
+				}
+				else if (rightheavy(node))
+				{
+					node = leftRotation(node);
+				}
 			}
-			else if (rightheavy(node))
-			{
-				std::cout << node->_right->_data.first << std::endl;
-				// rightRotation(node->_right);
-
-			}
+			return node;
 		}
 
 		_node   *insert_node(_node *node, T item){
@@ -173,14 +164,13 @@ namespace ft{
 				node->_right->_parent = node;
 			}
 			node->height = 1 + (max(getHeight(node->_left), getHeight(node->_right)));
-			balanceTree(node);
+			node = balanceTree(node);
 			return node;
 		}
 
-		void insert(T item){
+		void	insert(T item){
 		   if(isempty())
 			{
-				// FIXME  need to call the constructor;
 				T nothing = T();
 				_node *endNode = _allocator.allocate(1);
 				_allocator.construct(endNode, nothing);
@@ -193,6 +183,73 @@ namespace ft{
 			{
 				_root = insert_node(_root, item);
 			}
+		}
+		_node *findNode(_node *node, T data)
+		{
+			if(_compare(data.first, node->_data.first))
+				node->_left = findNode(node->_left, data);
+			else if (data.first == node->_data.first)
+				return node;
+			else
+				node->_right = findNode(node->_right, data);
+			return NULL;
+		}
+
+		_node *search(T data){
+			if(isempty())
+				return NULL;
+			else
+				return (findNode(_root, data));
+		}
+
+		_node *getSubtreeMinimum(_node *subtree)
+		{
+			while(subtree->_left)
+				subtree = subtree->_left;
+			return subtree;
+		}
+
+		_node	*DeleteNodeWithBalancing(_node *node, T data)
+		{
+			if(_compare(data.first, node->_data.first))
+				node->_left = DeleteNodeWithBalancing(node->_left, data);
+			else if (data.first == node->_data.first)
+			{
+				_node *temp;
+				if (!(node->_left) || !(node->_right))
+				{
+					temp = node->_left ? node->_left : node->_right;
+					if (temp) // there is a child;
+						node = temp;
+					else // there is no child;
+					{
+						temp = node;
+						node = NULL;
+					}	
+					_allocator.deallocate(temp, 1);
+				}
+				else
+				{
+					_node *temp2;
+					temp = getSubtreeMinimum(node->_right);
+					node = temp;
+					node->_right = DeleteNodeWithBalancing(node->_right, temp->_data);
+				}
+			}
+			else
+				node->_right = DeleteNodeWithBalancing(node->_right, data);
+			if (node)
+				node->height = 1 + (max(getHeight(node->_left), getHeight(node->_right)));
+			node = balanceTree(node);
+			return node;
+		}
+
+		void	delete_node(T data)
+		{
+			if (isempty())
+				return;
+			else
+				DeleteNodeWithBalancing(_root, data);
 		}
 	};
 }
